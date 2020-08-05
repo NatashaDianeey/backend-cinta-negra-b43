373935d2877c6d554047af6e30894eace6441a51
natashaDianeey <ndianeey@gmail.com>
2020-08-04 19:49:13 -0500

    add cors

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT;
 const router = require("../routes");
 const fileUpload = require('express-fileupload');
+const cors = require('cors')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
